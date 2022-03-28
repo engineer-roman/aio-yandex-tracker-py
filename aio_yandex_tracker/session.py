fd6ef9d7ca4c2edccbf8891f8adea4d25da560c2
Roman Zuykov <rom4n364@gmail.com>
2022-03-28 21:31:21 +0300

    add http request wrapper

--- aio_yandex_tracker/session.py
+++ aio_yandex_tracker/session.py
@@ -1,8 +1,8 @@
 from typing import Optional, Union
 
-from aio_yandex_tracker import const
+from aio_yandex_tracker import const, errors
 from aio_yandex_tracker.types import HEADERS_OBJECT
-from aiohttp import ClientSession
+from aiohttp import ClientResponse, ClientSession
 
 
 class HttpSession:
@@ -29,14 +29,73 @@ class HttpSession:
             headers=self.headers
         )
 
-    # async def request(self, method: str, url: str, *args, **kwargs):
-    #
-    #     pass
-    #
-    # async def __send_request(self, method: str, url: str, *args, **kwargs):
-    #     if not self.is_active:
-    #         raise errors.SessionIsNotInitialized
-    #     return await self.__session
+    async def request(
+        self, method: str, url: str, *args, **kwargs
+    ) -> ClientResponse:
+        retry = 0
+        retry_limit = 0
+        response = await self.__send_request(method, url, *args, **kwargs)
+        while retry < retry_limit and self.retry_needed(response):
+            response = await self.__send_request(method, url, *args, **kwargs)
+            if not self.retry_needed(response):
+                break
+            retry += 1
+            # FIXME add logging
+
+        await self.validate_http_response(response)
+        return response
+
+    async def __send_request(
+        self, method: str, url: str, *args, **kwargs
+    ) -> ClientResponse:
+        if not self.is_active:
+            raise errors.SessionIsNotInitialized(
+                "Instance session is not active. Re-create instance"
+            )
+        http_method = self.validate_http_method(self.__session, method)
+        try:
+            return await http_method(url, *args, **kwargs)
+        except Exception as exc:
+            raise errors.ApiUnknownError(f"{exc.__class__.__name__} - {exc}")
+
+    @staticmethod
+    def retry_needed(response: ClientResponse) -> bool:
+        if response.status in const.RESPONSE_CODES_RETRY:
+            return True
+        return False
+
+    @staticmethod
+    async def validate_http_response(
+        response: ClientResponse, encoding="utf-8"
+    ) -> None:
+        if response.status in const.RESPONSE_CODES_OK:
+            return
+
+        try:
+            error_body = await response.json(encoding=encoding)
+        except Exception:
+            # FIXME log exception
+            error_body = None
+
+        exc_class = errors.HTTP_ERRORS_MAPPING.get(
+            response.status, errors.ApiUnavailableError
+        )(
+            f"API Error received for URL {response.url}",
+            response.status,
+            response.reason,
+            error_body,
+        )
+        raise exc_class
+
+    @staticmethod
+    def validate_http_method(session, method):
+        if method in const.HTTP_METHODS:
+            result = getattr(session, method, None)
+            if result:
+                return result
+        raise errors.UnknownHttpMethod(
+            f"Unknown method for HTTP Session: {method}"
+        )
 
     @property
     def is_active(self):
