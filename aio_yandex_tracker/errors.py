ac06c62c6c0416a6333ab001cb322002117f5b88
Roman Zuykov <rom4n364@gmail.com>
2022-03-28 21:29:28 +0300

    rework http errors

--- aio_yandex_tracker/errors.py
+++ aio_yandex_tracker/errors.py
@@ -1,12 +1,26 @@
+from http import HTTPStatus
+from typing import Any
+
+
 class ApiUnknownError(BaseException):
     pass
 
 
 class HTTPError(BaseException):
+    def __init__(
+        self, message: str, status_code: int, reason: str, response_body: Any
+    ):
+        super().__init__(message)
+        self.status_code = status_code
+        self.reason = reason
+        self.response_body = response_body
+
+
+class AuthRequiredError(HTTPError):
     pass
 
 
-class ApiError(HTTPError):
+class NotFoundError(HTTPError):
     pass
 
 
@@ -28,3 +42,12 @@ class SessionIsNotInitialized(BaseException):
 
 class UnknownHttpMethod(BaseException):
     pass
+
+
+HTTP_ERRORS_MAPPING = {
+    HTTPStatus.BAD_REQUEST: ApiBadRequestError,
+    HTTPStatus.UNPROCESSABLE_ENTITY: IncorrectDataError,
+    HTTPStatus.UNAUTHORIZED: AuthRequiredError,
+    HTTPStatus.FORBIDDEN: AuthRequiredError,
+    HTTPStatus.NOT_FOUND: NotFoundError,
+}
