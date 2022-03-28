fa25de28d1118dd1efb1e5821d5bec7a0a025b24
Roman Zuykov <rom4n364@gmail.com>
2022-03-28 21:30:04 +0300

    rework RESPONSE_CODES in consts via http.HTTPStatus

--- aio_yandex_tracker/const.py
+++ aio_yandex_tracker/const.py
@@ -1,11 +1,21 @@
 from enum import Enum
+from http import HTTPStatus
 
 API_HEADERS_DEFAULT = {"Content-type": "application/json"}
 API_URL_ROOT = "https://api.tracker.yandex.net"
 
 HTTP_METHODS = ["get", "post", "put", "delete"]
-RESPONSE_CODES_OK = (200, 201, 204)
-RESPONSE_CODES_RETRY = (500, 502, 503, 504)
+RESPONSE_CODES_OK = (
+    HTTPStatus.OK,
+    HTTPStatus.CREATED,
+    HTTPStatus.NO_CONTENT,
+)
+RESPONSE_CODES_RETRY = (
+    HTTPStatus.INTERNAL_SERVER_ERROR,
+    HTTPStatus.BAD_GATEWAY,
+    HTTPStatus.SERVICE_UNAVAILABLE,
+    HTTPStatus.GATEWAY_TIMEOUT,
+)
 
 
 class API_VERSION_NAME(Enum):
